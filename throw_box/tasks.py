c2914cc6c268c79590c9695a4f2a736c5c973437
gcmalloc <gcmalloc@gmail.com>
2013-07-20 14:24:48 +0200

    vm state report

--- throw_box/tasks.py
+++ throw_box/tasks.py
@@ -1,6 +1,6 @@
 from test_box import GenericBox
 import os
-from celery import Celery
+from celery import Celery, current_task
 import config
 
 VAGRANT_FILENAME = "Vagrantfile"
@@ -9,19 +9,38 @@ celery = Celery('tasks', broker='amqp://guest@localhost//')
 
 @celery.task
 def test_job(pre, test, post, template, github_url):
-    """Launch a new test job.
+    """Launch a new test job. It will report a finer state from one of those:
+    * STARTING: the vm is starting
+    * SETUPING: the pre script are running one the vm
+    * TESTING: the pre script are running one the vm
+    * DEPLOYING: the post script are running one the vm
+    * DESTROYING: the vm is stopping.
+    * DESTROYING: the vm is stopping.
+    * FINISHEd: the vm is stopped and the job is finished
     """
     b = GenericBox(pre, test, post, github_url, template)
     try:
+        state('STARTING')
         b.up()
+        state('SETUPING')
         b.setup()
+        state('TESTING')
         b.test()
+        state('DEPLOYING')
         b.deploy()
     except Exception as e:
         print(e)
     finally:
+        state('DESTROYING')
         b.destroy()
+    state('FINISHED')
     return b.test_results
+
+def state(state, key='job_state'):
+    """Report a finer state of the job to the celery caller.
+    @param state: the state to set
+    """
+    current_task.update_state(meta={key: state})
 
 @celery.task
 def list_box():
