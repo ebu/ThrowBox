8f15180c2a2dc2281299306e8ec9969939fce9c2
gcmalloc <gcmalloc@gmail.com>
2013-07-24 13:51:17 +0200

    updating the test accordingly

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
-from throw_box import tasks
+import os
 from throw_box import test_box
 import unittest
-from unittest import skip
+
+DEFAULT_TEMPLATE = "ubuntu-12.04"
 
 class TestBoxThrower(unittest.TestCase):
     
     def test_box_test(self):
         """
         """
-        b = test_box.GenericBox(["echo bou"], [], [], "", "ubuntu-12.04")
+        b = test_box.GenericBox(["echo bou"], [], [], "", DEFAULT_TEMPLATE)
         b.up()
         b.setup()
         test_result = b.test()
@@ -25,7 +26,7 @@ class TestBoxThrower(unittest.TestCase):
     def test_bad_setup_script(self):
         """
         """
-        b = test_box.GenericBox(['true', 'false', 'echo bou'], [], [], "", "ubuntu-12.04")
+        b = test_box.GenericBox(['true', 'false', 'echo bou'], [], [], "", DEFAULT_TEMPLATE)
         b.up()
         b.setup()
         self.assertEqual(b.output, [['true', 'false']])
@@ -34,18 +35,19 @@ class TestBoxThrower(unittest.TestCase):
     def test_test_scripts(self):
         """
         """
-        b = test_box.GenericBox([], ["false", "true"], [], "", "ubuntu-12.04")
+        b = test_box.GenericBox([], ["false", "true"], [], "", DEFAULT_TEMPLATE)
         b.up()
         b.test()
         test_result = b.test_results
         self.assertEqual(test_result[0].passed, False)
         self.assertEqual(test_result[1].passed, True)
 
-    @skip('no celery by default')
-    def test_celery_job(self):
+    def test_clone_repository(self):
         """
         """
-        tasks.test_job.delay(["sudo apt-get update", "sudo apt-get install -y git", "sudo apt-get install -y python"], ["false"], [], "ubuntu-12.04", "")
+        b = test_box.GenericBox([], [], [], "ssh://git@github.com:ebu/ThrowBox.git", DEFAULT_TEMPLATE, private_key="~/.ssh/id_rsa")
+        b.clone_repo()
+        self.assertIn("README.md", os.listdir(b.directory))
 
 if __name__ == '__main__':
     unittest.main()
