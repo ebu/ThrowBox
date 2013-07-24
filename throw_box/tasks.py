bde3cb1816a3c9b7859c1b33e1d5de892472a3ad
gcmalloc <gcmalloc@gmail.com>
2013-07-24 11:06:32 +0200

    make the celery worker only compatible with django

--- throw_box/tasks.py
+++ throw_box/tasks.py
@@ -1,26 +1,30 @@
 from test_box import VirtualBox
 import os
 from celery import Celery, current_task
-import config
+from django.conf import settings
 
+"""This is the default FILENAME used by vagrant """
 VAGRANT_FILENAME = "Vagrantfile"
 
+"""the celery worker """
 celery = Celery('tasks') 
 
 @celery.task
-def test_job(pre, test, post, template, github_url):
+def test_job(setup_scripts, test_scripts, deploy_scripts, github_url, template):
     """Launch a new test job. It will report a finer state from one of those:
-    * STARTING: the vm is starting
-    * SETUPING: the pre script are running one the vm
-    * TESTING: the pre script are running one the vm
-    * DEPLOYING: the post script are running one the vm
+    * STARTING: the vm is starting.
+    * SETUPING: the pre script are running one the vm.
+    * TESTING: the pre script are running one the vm.
+    * DEPLOYING: the post script are running one the vm.
     * DESTROYING: the vm is stopping.
     * DESTROYING: the vm is stopping.
-    * FINISHED: the vm is stopped and the job is finished
+    * FINISHED: the vm is stopped and the job is finished.
     """
     state('STARTING')
-    box = VirtualBox(pre, test, post, github_url, template)
+    box = VirtualBox(setup_scripts, test_scripts, deploy_scripts, github_url, template, template_root=settings.VAGRANT_TEMPLATE_DIR)
     try:
+        if github_url:
+            box.clone_repo()
         box.up()
         state('SETUPING')
         box.setup()
@@ -47,4 +51,11 @@ def state(state, key='job_state'):
 def list_box():
     """List all the available templates.
     """
-    return os.path.lsdir(config.VAGRANT_TEMPLATE_DIR)
+    return os.path.lsdir(settings.VAGRANT_TEMPLATE_DIR)
+
+@celery.task
+def get_pub_key():
+    """Return the public key used by the fabric job to clone the repository.
+    """
+    with open(settings.THROWBOX_PUBKEY_FILE) as f:
+        return f.read()
