cb752caf8d3315e78325135247b5b37b2b0d4d0f
gcmalloc <gcmalloc@gmail.com>
2013-07-25 13:52:19 +0200

    adding test for getting the sha of the commit

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ class TestBoxThrower(unittest.TestCase):
     def test_box_test(self):
         """
         """
-        b = test_box.GenericBox(["echo bou"], [], [], "", DEFAULT_TEMPLATE)
+        b = test_box.VirtualBox(["echo bou"], [], [], "", DEFAULT_TEMPLATE)
         b.up()
         b.setup()
         test_result = b.test()
@@ -20,24 +20,25 @@ class TestBoxThrower(unittest.TestCase):
         """
         """
         with self.assertRaises(test_box.InvalidTemplate):
-            test_box.GenericBox([], [], [], "", "merglkjadsf")
+            test_box.VirtualBox([], [], [], "", "merglkjadsf")
 
 
     def test_bad_setup_script(self):
         """
         """
-        b = test_box.GenericBox(['true', 'false', 'echo bou'], [], [], "", DEFAULT_TEMPLATE)
+        b = test_box.VirtualBox(['true', 'true', 'false', 'echo bou'], [], [], "", DEFAULT_TEMPLATE)
         b.up()
         b.setup()
-        self.assertEqual(b.output, [['true', 'false']])
+        self.assertEqual(b.output, [['true', 'true', 'false']])
         
 
     def test_test_scripts(self):
         """
         """
-        b = test_box.GenericBox([], ["false", "true"], [], "", DEFAULT_TEMPLATE)
+        b = test_box.VirtualBox([], ["false", "true", "true"], [], "", DEFAULT_TEMPLATE)
         b.up()
         b.test()
+        b.deploy()
         test_result = b.test_results
         self.assertEqual(test_result[0].passed, False)
         self.assertEqual(test_result[1].passed, True)
@@ -45,9 +46,10 @@ class TestBoxThrower(unittest.TestCase):
     def test_clone_repository(self):
         """
         """
-        b = test_box.GenericBox([], [], [], "ssh://git@github.com:ebu/ThrowBox.git", DEFAULT_TEMPLATE, private_key="~/.ssh/id_rsa")
+        b = test_box.VirtualBox([], [], [], "git@github.com:ebu/ThrowBox.git", DEFAULT_TEMPLATE, private_key="~/.ssh/id_rsa")
         b.clone_repo()
-        self.assertIn("README.md", os.listdir(b.directory))
+        self.assertIn("README.md", os.listdir(os.path.join(b.directory, 'repo')))
+        self.assertEqual(len(b.top_commit), 40)
 
 if __name__ == '__main__':
     unittest.main()
