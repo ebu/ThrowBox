76382fb48308f023ff5aed657479a99aa3df1e4e
gcmalloc <gcmalloc@gmail.com>
2013-08-19 14:03:34 +0200

    multiple fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'throwbox',
-    version = '0.0.2',
+    version = '0.0.10',
     description = 'A tiny travis-ci clone with rabbit-mq push support and local run',
     long_description = open('README.md').read(),
     keywords = 'travis ci shell',
@@ -14,7 +14,7 @@ setup(
     url = 'https://github.com/ebu/ThrowBox',
     dependency_links = ['http://github.com/todddeluca/python-vagrant/tarball/master#egg=python-vagrant'],
     install_requires = ['python-vagrant', 'fabric', 'celery'],
-    py_modules = ['throw_box.test_box'],
+    py_modules = ['throw_box.test_box', 'throw_box.tasks'],
 
     classifiers=[
         'Development Status :: 3 - Alpha',
--- throw_box/tasks.py
+++ throw_box/tasks.py
@@ -1,8 +1,8 @@
-
 from throw_box import test_box
-from models import TestResult, TestRun, Repo
 import os
-from celery import Celery, current_task
+from celery.decorators import task
+import logging
+from celery import current_task
 try:
     from django.conf import settings
 except ImportError:
@@ -15,19 +15,17 @@ at least setting this settings:
     THROWBOX_TEMPLATE_DIR the location of the the vagrant files to use. Their location will 
 """
 
-"""the celery worker """
-celery = Celery('tasks') 
 
 
 
-@celery.task
+@task
 def list_box():
     """List all the available templates.
     """
     return os.path.lsdir(settings.THROWBOX_PUBKEY_FILE)
 
 
-@celery.task
+@task
 def get_pub_key():
     """Return the public key used by the fabric job to clone the repository.
     """
@@ -35,7 +33,7 @@ def get_pub_key():
         return f.read()
 
 
-@celery.task
+@task
 def test_job(setup_scripts, test_scripts, deploy_scripts, github_url, template, repo, build_index):
     """Launch a new test job. It will report a finer state from one of those:
     * INITIALISING: the system is starting
@@ -49,7 +47,6 @@ def test_job(setup_scripts, test_scripts, deploy_scripts, github_url, template, 
     * FINISHED: the vm is stopped and the job is finished.
     """
     state('INITIALISING')
-    logger = test_job.get_logger()
     box = test_box.VirtualBox(setup_scripts, test_scripts, deploy_scripts, github_url, template, template_dir=settings.THROWBOX_TEMPLATE_DIR, private_key=settings.THROWBOX_PRIVKEY_FILE)
     try:
         state('CLONING')
@@ -71,33 +68,22 @@ def test_job(setup_scripts, test_scripts, deploy_scripts, github_url, template, 
         state('DEPLOYING')
         box.deploy()
         outputs = box.output
-        logger.info(box.output[0])
-        logger.info(type(box.output))
+        logging.info(box.output[0])
+        logging.info(type(box.output))
     except Exception as e:
-        logger.error(e)
+        logging.error(e)
         return
     finally:
         state('DESTROYING')
         del(box)
     state('FINISHED')
-    repo = Repo.objects.get(pk=repo)
     outputs = ["\n".join(o) for o in outputs]
-    try:
-        test_run = TestRun.objects.get(repo=repo, commit=commit_sha, index=build_index)
-        test_run.setup_output = outputs[0]
-        test_run.test_output = outputs[1]
-        test_run.deploy_output = outputs[2]
-        #we already have a test_run, so we also remove old test_res
-        TestResult.objects.filter(run=test_run).delete()
-    except TestRun.DoesNotExist:
-        test_run = dict(repo=repo, commit=commit_sha, index=build_index, setup_output=outputs[0], test_output=outputs[1], deploy_output=outputs[2], commit_sha=commit_sha, commit_comment=commit_comment)
-    yield test_run
+    ret = []
+    ret.append(dict(repo=repo, commit=commit_sha, index=build_index, setup_output=outputs[0], test_output=outputs[1], deploy_output=outputs[2], commit_sha=commit_sha, commit_comment=commit_comment))
     
-    global_success = True
     for test_result in test_results:
-        global_success &= test_result.passed
-        yield dict(run=test_run, return_code=test_result.exit_code, success=test_result.passed)
-        yield global_success
+        ret.append(dict(return_code=test_result.exit_code, success=test_result.passed))
+    return ret
 
 
 def state(state, key='job_state'):
